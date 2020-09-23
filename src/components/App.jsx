2e180c7dd22fdab6769c1081e97fa649f3e84600
Ryan Weaver <ryanweaver@Ryans-MacBook-Pro.local>
2020-09-23 08:59:47 -0600

    Webpack is created and rendering

--- src/components/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function App(props) {
-  return(<div>goodbye</div>)
-}
-
-export default App
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,60 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-  entry: __dirname + '/src/index.jsx',
-  module: {
-    rules: [
-      {
-        test: [/\.jsx$/],
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react', '@babel/preset-env']
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }
-    ]
+  entry: './src/index.js',
+  output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      chunkFilename: '[id].js',
+      publicPath: ''
   },
-   output: {
-    filename: 'bundle.js',
-    path: __dirname + '/dist'
-  }
+  resolve: {
+      extensions: ['.js', '.jsx']
+  },
+  module: {
+      rules: [
+          {
+              test: /\.js$/,
+              loader: 'babel-loader',
+              exclude: /node_modules/
+          },
+          {
+              test: /\.css$/,
+              exclude: /node_modules/,
+              use: [
+                  { loader: 'style-loader' },
+                  {
+                      loader: 'css-loader',
+                      options: {
+                          modules: {
+                              localIdentName: "[name]__[local]___[hash:base64:5]",
+                          },
+                          sourceMap: true
+                      }
+                   },
+                   {
+                       loader: 'postcss-loader',
+                       options: {
+                           ident: 'postcss',
+                           plugins: () => [
+                               autoprefixer({})
+                           ]
+                       }
+                    }
+              ]
+          },
+          {
+              test: /\.(png|jpe?g|gif)$/,
+              loader: 'url-loader?limit=10000&name=img/[name].[ext]'
+          }
+      ]
+  },
+  plugins: [
+      new HtmlWebpackPlugin({
+          template: __dirname + '/src/index.html',
+          filename: 'index.html',
+          inject: 'body'
+      })
+  ]
 };
