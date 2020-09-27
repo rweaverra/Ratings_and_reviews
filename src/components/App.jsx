13153b9fc087cb9494ac0b08814d9229ef3ecc8a
Ryan Weaver <ryanweaver@Ryans-MacBook-Pro.local>
2020-09-27 07:13:42 -0600

    changed a dependency

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,47 +1,39 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Col';
 import Ratings from './Ratings.jsx';
 import ReviewList from './ReviewList.jsx';
-import {exampleReview, exampleRating} from './exampleReview.js';
-import axios from 'axios';
-
-
-
-
+import { exampleReview, exampleRating } from './exampleReview.js';
 
 function App(props) {
-
   const [reviews, setReviews] = useState([]);
   const [ratings, setRatings] = useState(exampleRating);
 
   useEffect(() => {
     axios.get('http://52.26.193.201:3000/reviews/1/list')
-  .then(response => {
-    setReviews(response.data.results);
-  }
-)
-  axios.get('http://52.26.193.201:3000/reviews/1/meta')
-    .then(response => {
-      setRatings(response.data);
-    })
+      .then((response) => {
+        setReviews(response.data.results);
+      });
+    axios.get('http://52.26.193.201:3000/reviews/1/meta')
+      .then((response) => {
+        setRatings(response.data);
+      });
   }, []);
 
-
-
-
- return (
-    <div className="container">
-      <div>
-        <Ratings ratings={ratings}/>
-        {console.log('ratings', ratings)}
-      </div>
-      <div>
-        <ReviewList example={reviews}/>s
-        {console.log('reviews', reviews)}
-      </div>
-      <div></div>
-    </div>
-  )
+  return (
+    <Container>
+      <Row>
+        <Col sm={6}>
+          <Ratings ratings={ratings} />
+        </Col>
+        <Col sm={6}>
+          <ReviewList example={reviews} />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default App;
-
