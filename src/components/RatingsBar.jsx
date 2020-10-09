3a3fadb03a12110bdf7a7f47eda3cae9ea67aa5c
Ryan Weaver <ryanweaver@Ryans-MacBook-Pro.local>
2020-10-08 20:27:03 -0600

    clicking star rating highlights it and sorts

--- src/components/RatingsBar.jsx
+++ src/components/RatingsBar.jsx
@@ -1,32 +1,39 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { LinearProgress } from '@material-ui/core';
 import { Container, Col, Row } from 'react-bootstrap';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 
-
 function RatingsBar({
   ratings, totalRatings, thisValue, sortStarRatings,
 }) {
-
-  const [clicked, setClicked] = useState(false);
+  const [clicked, setClicked] = useState(true);
   const percentageRatings = (ratings / totalRatings) * 100;
+  const [css, setCss] = useState("raw-rating-button")
 
+  const handleClick = () => {
+    //this will toggle the css to be used
 
-  function handleClick(event) {
-    console.log('clicked');
-  }
+    clicked ? setCss("raw-clicked-rating-button") : setCss("raw-rating-button");
+    setClicked(!clicked);
+    console.log('clicked', clicked);
+  };
 
   return (
     <Container>
 
-      <Row >
+      <Row>
 
         <span className="raw-ratings-first-column">
-          <button className="raw-rating-button" color="primary" type="button" value={thisValue} onClick={() => {sortStarRatings(thisValue); handleClick();}} >{thisValue} star </button>
-          </span>
-       <span>
-       <progress className="raw-progressBar"  max="100" value={percentageRatings}></progress>
-          </span>
+          <button className={css} color="primary" type="button" value={thisValue} onClick={() => { sortStarRatings(thisValue); handleClick(); }}>
+            {thisValue}
+            {' '}
+            star
+            {' '}
+          </button>
+        </span>
+        <span>
+          <progress className="raw-progressBar" max="100" value={percentageRatings} />
+        </span>
 
       </Row>
     </Container>
